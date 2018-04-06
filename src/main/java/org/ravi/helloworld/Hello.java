97738e32bf4e09c6e5ded44b5d573035f203b72f
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-04-06 10:34:05 +0530

    Changes in Hello.java file

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -1,10 +1,9 @@
 package org.ravi.helloworld;
 
 public class Hello {
-	
-	public String displayHelloMessage(String message){
-		message = "hi";
-		return message;
+
+	public static void main(String[] args) {
+		throw new UnsupportedOperationException();
 	}
 
 }
